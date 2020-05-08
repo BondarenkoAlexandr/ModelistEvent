a4d0b0fbb587c5faaff04c13dcdfc6533cef8138
Alexandr <pogrebnyak19999@gmail.com>
2020-05-08 17:19:16 +0300

    Добавил модальное окно, подправил мелкие нюансы по сайту

--- src/js/main.js
+++ src/js/main.js
@@ -15,4 +15,23 @@ document.addEventListener("DOMContentLoaded", function(event) {
             autoplaySpeed: 4000
         });
     });
+
+    function firsBtn(button) {
+        const headerBtn = document.querySelector(button),
+        modalWindow = document.querySelector('.modal')
+        headerBtn.addEventListener('click', () => {
+            modalWindow.style.top = window.pageYOffset + 'px';
+            modalWindow.style.width = '100%';
+            modalWindow.style.opacity = '1';
+            document.body.style.overflow = 'hidden';
+        })
+        modalWindow.addEventListener('click', (e) => {
+            if (e.target.classList.contains('modal')) {
+                modalWindow.style.width = '0';
+                modalWindow.style.opacity = '0';
+                document.body.style.overflowY = 'scroll';
+            }
+        })
+    }
+    firsBtn('.header-btn');
 });
